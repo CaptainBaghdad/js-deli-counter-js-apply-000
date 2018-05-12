eb9d7cdb59619377fd261a80c54667f27f114c73
Jason Pitts <juniorjr083@gmail.com>
2018-05-12 17:28:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function takeANumber(katzDeliLine, newPerson){
+  var num = katzDeliLine.length;
+  num++;
+	if (katzDeliLine.length === 0){
+    console.log('the line is empty');
+  }	
+   
+   katzDeliLine.push(newPerson);
+
+   return "Welcome, " + newPerson + "." + " " + "You are number " + num.toString() + " " + "in line" + ".";
+
+
+  
+
+
+
+
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!";
+  }
+  
+  return ""katzDeliLine[0];
+  
+}
+
